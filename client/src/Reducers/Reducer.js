fae2ed5110efff3496ced453fbcf7c008fc54052
gisellehargrove <gisellehargrove@gmail.com>
2019-08-14 07:58:56 -0400

    bootstrapped react app

---
+++ client/src/Reducers/Reducer.js
@@ -0,0 +1,16 @@
+// build a simple reducer function
+const reducer = (state, action) => {
+
+
+
+  return state;
+};
+
+const state = [
+  {
+    item: '',
+    completed: false,
+    id: 1
+  }];
+
+  export { reducer, state };
